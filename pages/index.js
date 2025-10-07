8f5353fce3ece89c506b6a29fda76b5e1af2015f
georgiaharvey <georgia.harvey@productboard.com>
2025-10-06 20:30:28 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,9 @@ import axios from "axios";
 import { parseAll } from "../lib/parseSheets";
 import dynamic from "next/dynamic";
 
-// Recharts components imported client-side only
-const Charts = dynamic(() => import("../src/Charts"), { ssr: false });
-
 export default function Home() {
   // All of your state and functions (like useState, useEffect) go here.
   // For example:
   const [data, setData] = useState(null);
-
-  // The component must return what you want to display on the page.
-  return (
-    <div>
-      <h1>My Dashboard</h1>
-      {/* All of your page content, charts, etc., go inside this div */}
-    </div>
-  );
 
 } // <--- You need to add this closing brace at the very end of the file.
