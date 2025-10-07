0e1dc46fd142c17d4672361c4c267ca475869b48
georgiaharvey <georgia.harvey@productboard.com>
2025-10-06 19:33:10 -0500

    Create sheets.js

---
+++ pages/api/sheets.js
@@ -0,0 +1,10 @@
+import { getSheetsData } from "../../../lib/sheetsClient.js";
+
+export default async function handler(req, res) {
+  try {
+    const data = await getSheetsData();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
